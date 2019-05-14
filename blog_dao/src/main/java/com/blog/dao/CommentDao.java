1ca24c5e57718eb46a88c5da9c5126d4ef5d3419
2501160233 <2233912503@qq.com>
2019-05-14 21:27:32 +0800

    评论

--- blog_dao/src/main/java/com/blog/dao/CommentDao.java
+++ blog_dao/src/main/java/com/blog/dao/CommentDao.java
@@ -8,8 +8,11 @@ import java.util.List;
 
 @Repository
 public interface CommentDao {
-
+    //统计所有评论
     List<Comment> selectByBlogId(@Param("id") Integer id);
 
     void insertComment(@Param("id") Integer id, @Param("p_content") String p_content, @Param("username") String username, @Param("image") String image);
+
+    //统计数量
+    Integer selectById(Integer blog_id);
 }
